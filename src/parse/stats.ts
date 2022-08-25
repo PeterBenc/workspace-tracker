093834cdeff549f8dffb0f3e27e8d8dc96702677
Peter Benc <peter.benc1@gmail.com>
2022-08-25 19:58:44 +0200

    Calculate total stats

--- src/parse/stats.ts
+++ src/parse/stats.ts
@@ -1,6 +1,6 @@
 import _ from "lodash";
 import { WorkPeriod } from "./types";
-import { getPeriodLength, secondsToTime } from "./utils";
+import { aggregateTimeStats, getPeriodLength, secondsToTime } from "./utils";
 
 export const getTimeSpentStats = (workPeriods: WorkPeriod[]) => {
   const workPeriodTimeStats = workPeriods.map(
@@ -9,15 +9,24 @@ export const getTimeSpentStats = (workPeriods: WorkPeriod[]) => {
       timeStat: getPeriodLength(startTime, endTime),
     })
   );
-  console.log(
-    _(workPeriodTimeStats)
-      .groupBy((x) => x.ticker)
-      .map((x, ticker) => ({
-        ticker,
-        timeStat: secondsToTime(
-          x.reduce((acc, prev) => acc + prev.timeStat, 0)
-        ),
-      }))
-      .value()
+  const workTimeSpendLogs = _(workPeriodTimeStats)
+    .groupBy((x) => x.ticker)
+    .map((x, ticker) => ({
+      ticker,
+      timeStat: secondsToTime(x.reduce((acc, prev) => acc + prev.timeStat, 0)),
+    }))
+    .value();
+  console.log(workTimeSpendLogs);
+  const { hours, seconds } = workTimeSpendLogs.reduce(
+    (acc, curr) => ({
+      hours: acc.hours + curr.timeStat.hours,
+      seconds: acc.seconds + curr.timeStat.minutes * 60 + curr.timeStat.seconds,
+    }),
+    { hours: 0, seconds: 0 }
   );
+  const totalTimeStats = aggregateTimeStats(
+    { hours, minutes: 0, seconds: 0 },
+    secondsToTime(seconds)
+  );
+  console.log(totalTimeStats);
 };
