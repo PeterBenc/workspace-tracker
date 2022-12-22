b91eddab1e9f45ecba4a754cd39ef3c35e9f4f92
Peter Benc <peter.benc1@gmail.com>
2022-12-22 20:14:45 +0100

    Update log batch size

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -2,7 +2,7 @@
 export const LOG_INTERVAL = 30000;
 
 // how many logs before updating the log file
-export const LOG_BATCH_SIZE = 10;
+export const LOG_BATCH_SIZE = 5;
 
 export const LOG_FILE_PATH = "/home/peter/Me/workspace-tracker/logs.txt";
 export const LOG_PARSE_FILE_PATH =
