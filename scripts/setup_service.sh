db7ba72c1123ecdc6872e585ce438049408ad342
Peter Benc <peter.benc1@gmail.com>
2022-12-22 20:15:16 +0100

    Increase service sleep

--- scripts/setup_service.sh
+++ scripts/setup_service.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo cp ../workspace_tracker.service /lib/systemd/system/workspace_tracker.service
+sudo cp workspace_tracker.service /lib/systemd/system/workspace_tracker.service
 sudo systemctl daemon-reload
